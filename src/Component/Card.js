490330acc845361fe9d4a6c7370e082b701522a8
labaran1 <adlabaran@gmail.com>
2021-01-12 13:22:55 -0500

    movie card

---
+++ src/Component/Card.js
@@ -0,0 +1,27 @@
+import React, {useContext} from 'react'
+import AppContext from '../context/appContext/AppContext'
+import './Card.css'
+
+export default function Card() {
+    const appContext = useContext(AppContext)
+    const { Year , Name, Poster, Nomination} = appContext;
+    return (
+        // <center></center>
+        <div className="card">
+            <img src={Poster} alt=""/>
+            <h2 style={{background:"#1d0f22"}}>{Name}</h2>
+            <h4 style={{background:"#1d0f22", color: "#ccc"}}>{Year}</h4>
+        
+         <div style={{background:"#1d0f22", color:"red" , display: "flex", justifyContent:"space-between"}}>
+             {/* {Nomination} */}
+<span  title="Nominate Movie"> <i class="fas fa-plus" style={{background:"#1d0f22", color: "red" , cursor:"pointer"}}></i>
+
+</span>
+<span  title="Add Movie to favourite"> <i class="far fa-heart" style={{background:"#1d0f22", color: "red" , cursor:"pointer"}}></i>
+
+</span>
+
+         </div>
+        </div>
+    )
+}
