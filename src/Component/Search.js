1846adf23efdd09f59839c98421b43ea6d0e3299
labaran1 <adlabaran@gmail.com>
2021-01-12 13:24:10 -0500

    search component

--- src/Component/Search.js
+++ src/Component/Search.js
@@ -1,9 +1,34 @@
-import React from 'react'
+import React,{useContext, useState} from 'react'
+import AppContext from '../context/appContext/AppContext'
+
 
 export default function Search() {
+    const [searchValue, setSearchValue] = useState("")
+
+    const appContext = useContext(AppContext)
+    const submitNow = (e)=>{
+        e.preventDefault()
+    
+        // alert(searchValue)
+        // console.log(appContext.Year);
+        appContext.searchResult(searchValue)
+    // console.log(appContext.Year);
+
+    }
+    const change = (e)=>{
+        e.preventDefault()
+        setSearchValue(e.target.value)
+        
+    }
     return (
-        <div>
-            
-        </div>
+        <div className="search">
+        <form className="search-form" onSubmit={submitNow}>
+            <input type="text" placeholder="Search by movie title ...." value={searchValue}  onChange={change}
+            />
+            <input type="submit" value="Search"/>
+        </form>
+
+       
+    </div>
     )
 }
