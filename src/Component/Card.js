627e7de88f97d2d11bf15f7991a0d0c70c16f835
labaran1 <adlabaran@gmail.com>
2021-01-15 11:05:11 -0500

    add disabled button

--- src/Component/Card.js
+++ src/Component/Card.js
@@ -4,7 +4,7 @@ import './Card.css'
 
 export default function Card() {
     const appContext = useContext(AppContext)
-    const { Year , Name, Poster} = appContext;
+    const { Year , Name, Poster , fav} = appContext;
     return (
         // <center></center>
         <div className="card">
@@ -13,10 +13,11 @@ export default function Card() {
             <h4 style={{background:"#1d0f22", color: "#ccc"}}>{Year}</h4>
         
          <span style={{background:"#1d0f22", color:"red" , display: "flex", justifyContent:"space-between"}}>
-<span  title="Nominate Movie"> <i class="fas fa-plus" style={{background:"#1d0f22", color: "red" , cursor:"pointer"}}></i>
+<span  title="Nominate Movie"> <i className="fas fa-plus" style={{background:"#1d0f22", color: "red" , cursor:"pointer"}}></i>
 
-</span>
-<span  title="Add Movie to favourite"> <i class="far fa-heart" style={{background:"#1d0f22", color: "red" , cursor:"pointer"}}></i>
+</span> 
+
+<span  title={(fav>=5)? "Can't Add movie to favourite": "Add Movie to favourite"}> <i className={(fav>=5)? "fas fa-ban" :"far fa-heart"} style={{background:"#1d0f22", color: "red" , cursor:`${(fav>=5)? "disabled": "pointer"}`       }}></i>
 
 </span>
 
